791980c759feba79f1770a4ac7f19aabe7a3e050
Gabby128Bit <105458760+Gabby128Bit@users.noreply.github.com>
2022-06-09 14:12:51 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+function validate() {
+
+var username=document.getElementById("Username").value;
+var password=document.getElementById("Password").value;
+if(username=="beta-tester"&& password=="1ud82Uwu2")
+{
+    alert("login completed")
+    return false;
+}
+else
+{
+    alert("wrong password or username")
+}
+
+
+
+
+
+
+}
